b660a2cce9f5032b8e2504bffc8b00bc2c579f31
TheFive7 <hennequin.maxime2412@gmail.com>
2021-11-29 15:48:45 +0100

    Modifications mineures

--- src/main/java/notes/s3_notes/Main.java
+++ src/main/java/notes/s3_notes/Main.java
@@ -4,11 +4,9 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) {
         Scene scene = new Scene(new Menu(), 800, 800);
         stage.setTitle("Notes S3");
         stage.setScene(scene);
@@ -19,8 +17,6 @@ public class Main extends Application {
     TODO
     - Avoir plusieurs utilisateurs, chacun son fichier de sauvegarde et ses notes
     */
-
-
 
     public static void main(String[] args) {
         launch();
